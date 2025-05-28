d37f3cf2cf28671e968ef6d906f1dc0fe4169cf9
scopeech <max260996@gmail.com>
2025-05-29 01:57:26 +0600

    first

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace wotblitzclusterselector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBlockC0_Click = new System.Windows.Forms.Button();
+            this.btnBlockC1_Click = new System.Windows.Forms.Button();
+            this.btnBlockC2_Click = new System.Windows.Forms.Button();
+            this.btnBlockC3_Click = new System.Windows.Forms.Button();
+            this.btnBlockC4_Click = new System.Windows.Forms.Button();
+            this.btnUnblockAll_Click = new System.Windows.Forms.Button();
+            this.btnShowPings_Click = new System.Windows.Forms.Button();
+            this.btnShowHosts_Click = new System.Windows.Forms.Button();
+            this.txtConsoleOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnBlockC0_Click
+            // 
+            this.btnBlockC0_Click.BackColor = System.Drawing.Color.White;
+            this.btnBlockC0_Click.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnBlockC0_Click.Location = new System.Drawing.Point(12, 12);
+            this.btnBlockC0_Click.Name = "btnBlockC0_Click";
+            this.btnBlockC0_Click.Size = new System.Drawing.Size(177, 59);
+            this.btnBlockC0_Click.TabIndex = 0;
+            this.btnBlockC0_Click.Text = "Разблокровать C0";
+            this.btnBlockC0_Click.UseVisualStyleBackColor = false;
+            this.btnBlockC0_Click.Click += new System.EventHandler(this.btnBlockC0_Click_Click);
+            // 
+            // btnBlockC1_Click
+            // 
+            this.btnBlockC1_Click.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnBlockC1_Click.Location = new System.Drawing.Point(12, 95);
+            this.btnBlockC1_Click.Name = "btnBlockC1_Click";
+            this.btnBlockC1_Click.Size = new System.Drawing.Size(177, 59);
+            this.btnBlockC1_Click.TabIndex = 1;
+            this.btnBlockC1_Click.Text = "Разблокровать C1";
+            this.btnBlockC1_Click.UseVisualStyleBackColor = true;
+            this.btnBlockC1_Click.Click += new System.EventHandler(this.btnBlockC1_Click_Click);
+            // 
+            // btnBlockC2_Click
+            // 
+            this.btnBlockC2_Click.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnBlockC2_Click.Location = new System.Drawing.Point(12, 177);
+            this.btnBlockC2_Click.Name = "btnBlockC2_Click";
+            this.btnBlockC2_Click.Size = new System.Drawing.Size(177, 59);
+            this.btnBlockC2_Click.TabIndex = 2;
+            this.btnBlockC2_Click.Text = "Разблокровать C2";
+            this.btnBlockC2_Click.UseVisualStyleBackColor = true;
+            this.btnBlockC2_Click.Click += new System.EventHandler(this.btnBlockC2_Click_Click);
+            // 
+            // btnBlockC3_Click
+            // 
+            this.btnBlockC3_Click.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnBlockC3_Click.Location = new System.Drawing.Point(12, 261);
+            this.btnBlockC3_Click.Name = "btnBlockC3_Click";
+            this.btnBlockC3_Click.Size = new System.Drawing.Size(177, 59);
+            this.btnBlockC3_Click.TabIndex = 3;
+            this.btnBlockC3_Click.Text = "Разблокровать C3";
+            this.btnBlockC3_Click.UseVisualStyleBackColor = true;
+            this.btnBlockC3_Click.Click += new System.EventHandler(this.btnBlockC3_Click_Click);
+            // 
+            // btnBlockC4_Click
+            // 
+            this.btnBlockC4_Click.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnBlockC4_Click.Location = new System.Drawing.Point(12, 348);
+            this.btnBlockC4_Click.Name = "btnBlockC4_Click";
+            this.btnBlockC4_Click.Size = new System.Drawing.Size(177, 59);
+            this.btnBlockC4_Click.TabIndex = 4;
+            this.btnBlockC4_Click.Text = "Разблокровать C4";
+            this.btnBlockC4_Click.UseVisualStyleBackColor = true;
+            this.btnBlockC4_Click.Click += new System.EventHandler(this.btnBlockC4_Click_Click);
+            // 
+            // btnUnblockAll_Click
+            // 
+            this.btnUnblockAll_Click.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnUnblockAll_Click.Location = new System.Drawing.Point(212, 12);
+            this.btnUnblockAll_Click.Name = "btnUnblockAll_Click";
+            this.btnUnblockAll_Click.Size = new System.Drawing.Size(242, 125);
+            this.btnUnblockAll_Click.TabIndex = 5;
+            this.btnUnblockAll_Click.Text = "Разблокировать все сервера";
+            this.btnUnblockAll_Click.UseVisualStyleBackColor = true;
+            this.btnUnblockAll_Click.Click += new System.EventHandler(this.btnUnblockAll_Click_Click);
+            // 
+            // btnShowPings_Click
+            // 
+            this.btnShowPings_Click.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnShowPings_Click.Location = new System.Drawing.Point(212, 166);
+            this.btnShowPings_Click.Name = "btnShowPings_Click";
+            this.btnShowPings_Click.Size = new System.Drawing.Size(242, 125);
+            this.btnShowPings_Click.TabIndex = 6;
+            this.btnShowPings_Click.Text = "Посмотреть пинг серверов";
+            this.btnShowPings_Click.UseVisualStyleBackColor = true;
+            this.btnShowPings_Click.Click += new System.EventHandler(this.btnShowPings_Click_Click);
+            // 
+            // btnShowHosts_Click
+            // 
+            this.btnShowHosts_Click.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnShowHosts_Click.Location = new System.Drawing.Point(212, 315);
+            this.btnShowHosts_Click.Name = "btnShowHosts_Click";
+            this.btnShowHosts_Click.Size = new System.Drawing.Size(242, 92);
+            this.btnShowHosts_Click.TabIndex = 7;
+            this.btnShowHosts_Click.Text = "Открыть файл hosts";
+            this.btnShowHosts_Click.UseVisualStyleBackColor = true;
+            this.btnShowHosts_Click.Click += new System.EventHandler(this.btnShowHosts_Click_Click);
+            // 
+            // txtConsoleOutput
+            // 
+            this.txtConsoleOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtConsoleOutput.Location = new System.Drawing.Point(471, 12);
+            this.txtConsoleOutput.Multiline = true;
+            this.txtConsoleOutput.Name = "txtConsoleOutput";
+            this.txtConsoleOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtConsoleOutput.Size = new System.Drawing.Size(328, 409);
+            this.txtConsoleOutput.TabIndex = 8;
+            this.txtConsoleOutput.TextChanged += new System.EventHandler(this.txtConsoleOutput_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtConsoleOutput);
+            this.Controls.Add(this.btnShowHosts_Click);
+            this.Controls.Add(this.btnShowPings_Click);
+            this.Controls.Add(this.btnUnblockAll_Click);
+            this.Controls.Add(this.btnBlockC4_Click);
+            this.Controls.Add(this.btnBlockC3_Click);
+            this.Controls.Add(this.btnBlockC2_Click);
+            this.Controls.Add(this.btnBlockC1_Click);
+            this.Controls.Add(this.btnBlockC0_Click);
+            this.Name = "Form1";
+            this.Text = "WotBlitzClusterSelector";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBlockC0_Click;
+        private System.Windows.Forms.Button btnBlockC1_Click;
+        private System.Windows.Forms.Button btnBlockC2_Click;
+        private System.Windows.Forms.Button btnBlockC3_Click;
+        private System.Windows.Forms.Button btnBlockC4_Click;
+        private System.Windows.Forms.Button btnUnblockAll_Click;
+        private System.Windows.Forms.Button btnShowPings_Click;
+        private System.Windows.Forms.Button btnShowHosts_Click;
+        private System.Windows.Forms.TextBox txtConsoleOutput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Windows.Forms;
+
+namespace wotblitzclusterselector
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, string> servers = new Dictionary<string, string>()
+        {
+            { "EU_C0 (Нидерланды)", "login0.wotblitz.eu" },
+            { "EU_C1 (Нидерланды)", "login1.wotblitz.eu" },
+            { "EU_C2 (Германия)", "login2.wotblitz.eu" },
+            { "EU_C3 (Польша)", "login3.wotblitz.eu" },
+            { "EU_C4 (Казахстан)", "login4.wotblitz.eu" }
+        };
+
+        private string replacementIp = "0.0.0.0";
+        private string hostsFilePath = @"C:\Windows\System32\drivers\etc\hosts";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Log(string message)
+        {
+            txtConsoleOutput.AppendText($"{DateTime.Now:T} — {message}{Environment.NewLine}");
+        }
+
+        private void BlockServer(string hostname)
+        {
+            var lines = File.ReadAllLines(hostsFilePath);
+            if (!lines.Any(line => line.Contains(hostname)))
+            {
+                File.AppendAllText(hostsFilePath, $"{replacementIp} {hostname}{Environment.NewLine}");
+                Log($"Сервер {hostname} заблокирован.");
+            }
+            else
+            {
+                Log($"Сервер {hostname} уже заблокирован.");
+            }
+        }
+
+        private void UnblockAllServers()
+        {
+            var lines = File.ReadAllLines(hostsFilePath);
+            var newLines = new List<string>();
+            foreach (var line in lines)
+            {
+                if (!servers.Values.Any(host => line.Contains(host)))
+                {
+                    newLines.Add(line);
+                }
+            }
+            File.WriteAllLines(hostsFilePath, newLines);
+            Log("Все серверы разблокированы.");
+        }
+
+        private async void ShowPings()
+        {
+            Ping pingSender = new Ping();
+            foreach (var pair in servers)
+            {
+                try
+                {
+                    PingReply reply = await pingSender.SendPingAsync(pair.Value, 1000);
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        Log($"{pair.Key} — {reply.RoundtripTime} ms");
+                    }
+                    else
+                    {
+                        Log($"{pair.Key} — Не отвечает");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log($"{pair.Key} — Ошибка: {ex.Message}");
+                }
+            }
+        }
+
+        private void ShowCurrentHosts()
+        {
+            try
+            {
+                string hostsPath = @"C:\Windows\System32\drivers\etc\hosts";
+                System.Diagnostics.Process.Start("notepad.exe", hostsPath);
+                Log("Открытие файла hosts прошло успешно.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Не удалось открыть файл hosts:\n" + ex.Message);
+            }
+        }
+
+        private void btnBlockC0_Click_Click(object sender, EventArgs e)
+        {
+            BlockServer("login0.wotblitz.eu");
+        }
+
+        private void btnBlockC1_Click_Click(object sender, EventArgs e)
+        {
+            BlockServer("login1.wotblitz.eu");
+        }
+
+        private void btnBlockC2_Click_Click(object sender, EventArgs e)
+        {
+            BlockServer("login2.wotblitz.eu");
+        }
+
+        private void btnBlockC3_Click_Click(object sender, EventArgs e)
+        {
+            BlockServer("login3.wotblitz.eu");
+        }
+
+        private void btnBlockC4_Click_Click(object sender, EventArgs e)
+        {
+            BlockServer("login4.wotblitz.eu");
+        }
+
+        private void btnUnblockAll_Click_Click(object sender, EventArgs e)
+        {
+            UnblockAllServers();
+        }
+
+        private void btnShowPings_Click_Click(object sender, EventArgs e)
+        {
+            ShowPings();
+        }
+
+        private void btnShowHosts_Click_Click(object sender, EventArgs e)
+        {
+            ShowCurrentHosts();
+        }
+        private void txtConsoleOutput_TextChanged(object sender, EventArgs e)
+        {
+            // Можно оставить пустым или добавить лог, если нужно
+        }
+
+    }
+}
